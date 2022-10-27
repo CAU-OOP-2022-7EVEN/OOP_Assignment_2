c0bd34a4b605c1dabb1558806ea356db2edc9ddb
yymin1022 <yymin1022@gmail.com>
2022-10-28 02:00:53 +0900

    Update : Applied Base Code from Bongbong

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+#include "inf_int.h"
 
 using namespace std;
 
-int main() {
+int main(){
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(false);
 
-    cout << "Hello, World!" << "\n";
-    
+    inf_int a, e;
+    inf_int b(100);
+    inf_int c("321111111111122222222222233333333333444444444445555555555");
+    inf_int d("123451987651234572749499923455022211");
+    inf_int f=d;
+    inf_int g(f);
+
+    // cin >> g ;   // not required
+
+    a=b*c;
+    // e=g/f;       // not required
+
+    b=c-d;
+
+
+    if (f==d) {
+        cout << "a : " << a << endl;
+        cout << "b : " << b << endl;
+        cout << "c : " << c << endl;
+        cout << "d : " << d << endl;
+        // cout << "e : " << e << endl;
+        cout << "f : " << f << endl;
+        // cout << "g : " << g << endl;
+    }
+
     return 0;
 }
