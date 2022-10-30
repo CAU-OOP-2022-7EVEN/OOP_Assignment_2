ad2c3b25a48c46ed0e22393720bc649d4c083b94
CHAE <chaso1495@naver.com>
2022-10-30 22:52:46 +0900

    헤더 코드 수정

--- inf_int.h
+++ inf_int.h
@@ -31,8 +31,8 @@ public:
     friend inf_int operator+(const inf_int &, const inf_int &);
     friend inf_int operator-(const inf_int &, const inf_int &);
     friend inf_int operator*(const inf_int &, const inf_int &);
-    void Add(const char num, const unsigned int index);
-    void Sub(const char num, const unsigned int index);
+    void inf_int::Add(const char num, const unsigned int index);
+    void inf_int::Sub(const char num, const unsigned int index);
     // friend inf_int operator/(const inf_int& , const inf_int&); // not required
     friend ostream &operator<<(ostream &, const inf_int &);
     // friend istream& operator>>(istream& , inf_int&);    // not required
