c411da341387a5b95342db99e71dd2e18467024f
yymin1022 <yymin1022@gmail.com>
2022-11-02 13:50:24 +0900

    Added MainMenu QT Files

---
+++ QT_UI/mainmenu.cpp
@@ -0,0 +1,12 @@
+#include "mainmenu.h"
+#include "ui_MainMenu.h"
+
+
+MainMenu::MainMenu(QWidget *parent) :
+        QWidget(parent), ui(new Ui::MainMenu) {
+    ui->setupUi(this);
+}
+
+MainMenu::~MainMenu() {
+    delete ui;
+}
---
+++ QT_UI/mainmenu.h
@@ -0,0 +1,24 @@
+#ifndef OOP_ASSIGNMENT_2_MAINMENU_H
+#define OOP_ASSIGNMENT_2_MAINMENU_H
+
+#include <QWidget>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainMenu; }
+QT_END_NAMESPACE
+
+class MainMenu : public QWidget {
+Q_OBJECT
+
+public:
+    explicit MainMenu(QWidget *parent = nullptr);
+
+    ~MainMenu() override;
+
+private:
+    Ui::MainMenu *ui;
+};
+
+
+#endif //OOP_ASSIGNMENT_2_MAINMENU_H
