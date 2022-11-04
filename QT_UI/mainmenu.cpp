4ed4ad7cb329e9feecdaceebb69564f662e0fd11
yymin1022 <yymin1022@gmail.com>
2022-11-04 14:25:41 +0900

    Update mainmenu.cpp : Added inf_int Calculation Logic

--- QT_UI/mainmenu.cpp
+++ QT_UI/mainmenu.cpp
@@ -1,6 +1,8 @@
 #include "mainmenu.h"
 #include "ui_MainMenu.h"
 
+#include "inf_int.h"
+
 
 MainMenu::MainMenu(QWidget *parent) :
         QWidget(parent), ui(new Ui::MainMenu) {
@@ -12,17 +14,29 @@ MainMenu::~MainMenu() {
 }
 
 void MainMenu::on_btnPlus_clicked() {
-    ui->labelResult->setText("Plus Clicked");
+    inf_int num1(ui->inputNum1->text().toStdString().c_str());
+    inf_int num2(ui->inputNum2->text().toStdString().c_str());
+    inf_int numResult = num1 + num2;
+    ui->labelResult->setText(QString(numResult.getResultChar().c_str()));
 }
 
 void MainMenu::on_btnMinus_clicked() {
-    ui->labelResult->setText("Minus Clicked");
+    inf_int num1(ui->inputNum1->text().toStdString().c_str());
+    inf_int num2(ui->inputNum2->text().toStdString().c_str());
+    inf_int numResult = num1 - num2;
+    ui->labelResult->setText(QString(numResult.getResultChar().c_str()));
 }
 
 void MainMenu::on_btnMultiply_clicked() {
-    ui->labelResult->setText("Multiply Clicked");
+    inf_int num1(ui->inputNum1->text().toStdString().c_str());
+    inf_int num2(ui->inputNum2->text().toStdString().c_str());
+    inf_int numResult = num1 * num2;
+    ui->labelResult->setText(QString(numResult.getResultChar().c_str()));
 }
 
 void MainMenu::on_btnDivision_clicked() {
-    ui->labelResult->setText("Division Clicked");
+    inf_int num1(ui->inputNum1->text().toStdString().c_str());
+    inf_int num2(ui->inputNum2->text().toStdString().c_str());
+    inf_int numResult = num1 / num2;
+    ui->labelResult->setText(QString(numResult.getResultChar().c_str()));
 }
