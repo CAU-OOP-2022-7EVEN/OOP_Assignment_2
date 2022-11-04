a19db387d566541dd309ab0105ec20e11f7f7194
euije <exitfill1234@naver.com>
2022-11-04 18:21:22 +0900

    Feat main.cpp : Update test code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "inf_int.h"
+#include "inf_int_test.h"
 
 using namespace std;
 
@@ -8,25 +9,8 @@ int main(){
     cout.tie(0);
     ios_base::sync_with_stdio(false);
 
-    inf_int a, e;
-    inf_int b(100);
-    inf_int c("-123451987651234572749499923455022211");
-    inf_int d("-321111111111122222222222233333333333444444444445555555555");
-    inf_int f=d;
-    inf_int g(f);
-
-    a=b*c;
-    b=c-d;
-
-    if(f == d){
-        cout << "a : " << a << endl;
-        cout << "b : " << b << endl;
-        cout << "c : " << c << endl;
-        cout << "d : " << d << endl;
-        cout << "e : " << e << endl;
-        cout << "f : " << f << endl;
-        cout << "g : " << g << endl;
-    }
+    inf_int_test::TestDefault();
+    inf_int_test::TestOperate100digitsWith50digits();
 
     return 0;
 }
