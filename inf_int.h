2371a2c9fd4e3988479fc9c0ccb51c9d651d82d3
yymin1022 <yymin1022@gmail.com>
2022-11-06 02:59:25 +0900

    Update inf_int.h : Cleaed UP

--- inf_int.h
+++ inf_int.h
@@ -11,14 +11,14 @@ private:
     unsigned int length;
     bool thesign; // Positive is true, Negative is false
 
-    void Add(const int num, const unsigned int index); // Function for Multiply Logic
-    void Add(const char num, const unsigned int index); // public members
-    void Sub(const char num, const unsigned int index); // void inf_int::Add(~~~) <-> void Add(~~~)
+    void Add(int num, unsigned int index); // Function for Multiply Logic
+    void Add(char num, unsigned int index); // public members
+    void Sub(char num, unsigned int index); // void inf_int::Add(~~~) <-> void Add(~~~)
 
 public:
     inf_int();                // Assign 0 as Default Value
-    inf_int(int);
-    inf_int(const char *);
+    explicit inf_int(int);
+    explicit inf_int(const char *);
     inf_int(const inf_int &); // Copy Constructor
     ~inf_int();               // Destructor
 
@@ -36,8 +36,8 @@ public:
     friend inf_int operator%(const inf_int &, const inf_int &);
     friend ostream &operator<<(ostream &, const inf_int &);
 
-    inf_int subInfInt(const unsigned int startIndex, const unsigned int endIndex);
-    inf_int pow(const unsigned int exponent);
+    inf_int subInfInt(unsigned int startIndex, unsigned int endIndex);
+    inf_int pow(unsigned int exponent);
 
     string getResultChar();
 };
