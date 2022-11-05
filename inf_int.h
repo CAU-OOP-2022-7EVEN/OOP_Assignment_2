7491135fa444b07febee39c49f6d2715b32a02c7
yymin1022 <yymin1022@gmail.com>
2022-11-06 02:27:57 +0900

    Update inf_int.h : Cleaed UP

--- inf_int.h
+++ inf_int.h
@@ -2,29 +2,27 @@
 #define _INF_INT_H_
 
 #include <iostream>
+
 using namespace std;
 
-class inf_int
-{
+class inf_int{
 private:
-    char *digits; // You may modify this to "string digits;" if you want.
+    char *digits;
     unsigned int length;
-    bool thesign; // true if positive , false if negative.
-    // ex) 15311111111111111 -> digits="11111111111111351", length=17, thesign=true;
-    // ex) -12345555555555 -> digits="55555555554321", length=14, thesign=false
-    // You may modify or add private members of inf_int class. So, it is OK to insert Add() private member function in inf_int class. However, You may not modify public members of inf_int class.
-    void Add(const int num, const unsigned int index); // made for multiply case
+    bool thesign; // Positive is true, Negative is false
+
+    void Add(const int num, const unsigned int index); // Function for Multiply Logic
     void Add(const char num, const unsigned int index); // public members
-    void Sub(const char num, const unsigned int index); // void inf_int::Add(~~~) <-> void Add(~~~) -->
+    void Sub(const char num, const unsigned int index); // void inf_int::Add(~~~) <-> void Add(~~~)
 
 public:
-    inf_int(); // assign 0 as a default value
+    inf_int();                // Assign 0 as Default Value
     inf_int(int);
     inf_int(const char *);
-    inf_int(const inf_int &); // copy constructor
-    ~inf_int();               // destructor
+    inf_int(const inf_int &); // Copy Constructor
+    ~inf_int();               // Destructor
 
-    inf_int &operator=(const inf_int &); // assignment operator
+    inf_int &operator=(const inf_int &); // Assignment Operator
 
     friend bool operator==(const inf_int &, const inf_int &);
     friend bool operator!=(const inf_int &, const inf_int &);
@@ -37,7 +35,6 @@ public:
     friend inf_int operator/(const inf_int &, const inf_int &);
     friend inf_int operator%(const inf_int &, const inf_int &);
     friend ostream &operator<<(ostream &, const inf_int &);
-    // friend istream& operator>>(istream& , inf_int&);    // not required
 
     inf_int subInfInt(const unsigned int startIndex, const unsigned int endIndex);
     inf_int pow(const unsigned int exponent);
